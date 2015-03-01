32362399c91e60e998da411331c1e5b8d3097cd6
Chase McCarthy <chase@code0100fun.com>
2015-03-01 00:08:11 -0500

    Reapply 0.2.x upgrade

--- ember-addon-main.js
+++ ember-addon-main.js
@@ -1,19 +1,33 @@
 'use strict';
 
 var hbarsCompile = require('./index.js')
+var checker = require('ember-cli-version-checker');
 
 module.exports = {
   name: 'ember-cli-hbars',
-  included: function (app) {
+
+  shouldSetupRegistryInIncluded: function() {
+    return !checker.isAbove(this, '0.2.0');
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
     var self = this;
-    this._super.included.apply(this, arguments);
-    app.registry.add('template', {
+
+    registry.add('template', {
       name: 'ember-cli-hbars',
       ext: ['haml', 'hbars'],
       toTree: function(tree) {
         return hbarsCompile(tree);
       }
-    })
+    });
+  },
 
+  included: function (app) {
+    var self = this;
+    this._super.included.apply(this, arguments);
+
+    if (this.shouldSetupRegistryInIncluded()) {
+      this.setupPreprocessorRegistry('parent', app.registry);
+    }
   }
 }
